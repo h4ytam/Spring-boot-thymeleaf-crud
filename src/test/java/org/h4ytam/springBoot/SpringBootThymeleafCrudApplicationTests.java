ea5fb40b8c55a80d774ee4794e3320f22acaf3de
h4ytam <haytamkira@gmail.com>
2020-10-26 03:48:23 +0000

    initialize the repo :building_construction:

---
+++ src/test/java/org/h4ytam/springBoot/SpringBootThymeleafCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.h4ytam.springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
